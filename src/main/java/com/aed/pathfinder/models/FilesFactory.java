2f099d5f0e0c53d6fce49441d7726849337085a5
Frank López <flopez@codisa.com>
2024-06-24 19:30:43 -0500

    Subiendo cambios para Luvi

---
+++ src/main/java/com/aed/pathfinder/models/FilesFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aed.pathfinder.models;
+
+import java.nio.file.Files;
+
+/**
+ *
+ * @author kterv
+ */
+public class FilesFactory {
+    private Files getFiles(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    } 
+}
