2fd52c8102b4162607ddd2cd4a0f746d83166164
Janluvi <janluvigbr@gmail.com>
2024-06-24 20:07:21 -0500

    subiendo FileSystem

---
+++ src/main/java/com/aed/pathfinder/models/FileSystem.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aed.pathfinder.models;
+
+/**
+ *
+ * @author JANLUVI
+ */
+public class FileSystem {
+
+    public FileSystem() {
+    }
+    
+     public FileSystem getInstance(){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+     
+      public void createSAVFile(){
+    throw new UnsupportedOperationException("Not supported yet.");
+    }
+      public void exportRecursively(FileComponent fileComponent, File destination){
+    throw new UnsupportedOperationException("Not supported yet.");
+    }
+      public void exportFileOrDirectory(String filename, String path){
+    throw new UnsupportedOperationException("Not supported yet.");
+    }
+      
+      public void importSAVFile(String path){
+    throw new UnsupportedOperationException("Not supported yet.");
+    }
+      
+      public void saveSAVFile(String path){
+    throw new UnsupportedOperationException("Not supported yet.");
+    }
+      
+      
+    
+}
